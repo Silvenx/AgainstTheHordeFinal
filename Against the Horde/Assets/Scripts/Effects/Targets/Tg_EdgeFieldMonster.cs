e8df8e6b034cc32fc8114944294f86b763ec182b
kylejessetaylor <kyle.jesse.taylor@gmail.com>
2024-10-02 18:29:41 +1000

    Added empty edgefieldmonster effect target.

---
+++ Against the Horde/Assets/Scripts/Effects/Targets/Tg_EdgeFieldMonster.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Target/Target_EdgeFieldMonsters")]
+public class Tg_EdgeFieldMonster : Target
+{
+    public override GameObject[] getTargets(GameObject thisCard = null)
+    {
+        GameObject o = null;
+        return o;
+    }
+}
