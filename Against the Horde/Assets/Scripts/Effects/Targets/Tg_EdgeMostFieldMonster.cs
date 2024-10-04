954a70c66ef75a2cbabd2fa50abbea4eb8090a35
Jax <jaxompellow@gmail.com>
2024-10-04 23:33:45 +1000

    Card Changes

--- Against the Horde/Assets/Scripts/Effects/Targets/Tg_EdgeMostFieldMonster.cs
+++ Against the Horde/Assets/Scripts/Effects/Targets/Tg_EdgeMostFieldMonster.cs
@@ -119,72 +119,72 @@ public class Tg_EdgeMostFieldMonster : Target
 }
 
 
-    //public override GameObject[] TargetAquisition(GameObject thisCard = null)
-    //{
-    //    FieldManager fieldManager = GameManager.Instance.fieldManager;
+//public override GameObject[] TargetAquisition(GameObject thisCard = null)
+//{
+//    FieldManager fieldManager = GameManager.Instance.fieldManager;
 
-    //    //Get all active player monsters
-    //    List<GameObject> playerMonsterList = fieldManager.getAllPlayerMonsters();
-    //    //Get all active horde monsters
-    //    List<GameObject> hordeMonsterList = fieldManager.getAllHordeMonsters();
+//    //Get all active player monsters
+//    List<GameObject> playerMonsterList = fieldManager.getAllPlayerMonsters();
+//    //Get all active horde monsters
+//    List<GameObject> hordeMonsterList = fieldManager.getAllHordeMonsters();
 
-    //    ///List to return
-    //    List<GameObject> target = new List<GameObject>();
+//    ///List to return
+//    List<GameObject> target = new List<GameObject>();
 
-    //    //Which side of the field
-    //    switch (targetSide)
-    //    {
-    //        //Leftmost
-    //        case WhichEdge.LEFTMOST:
+//    //Which side of the field
+//    switch (targetSide)
+//    {
+//        //Leftmost
+//        case WhichEdge.LEFTMOST:
 
-    //            switch (targetSideOfField)
-    //            {
-    //                case TargetMonsterField.PLAYER:
-    //                    //Add left most to the list
-    //                    target.Add(playerMonsterList[0]);
-    //                    break;
+//            switch (targetSideOfField)
+//            {
+//                case TargetMonsterField.PLAYER:
+//                    //Add left most to the list
+//                    target.Add(playerMonsterList[0]);
+//                    break;
 
-    //                case TargetMonsterField.HORDE:
-    //                    //Add left most to the list
-    //                    target.Add(hordeMonsterList[0]);
-    //                    break;
+//                case TargetMonsterField.HORDE:
+//                    //Add left most to the list
+//                    target.Add(hordeMonsterList[0]);
+//                    break;
 
-    //                case TargetMonsterField.BOTH:
-    //                    //Add left most to the list
-    //                    target.Add(playerMonsterList[0]);
-    //                    //Add left most to the list
-    //                    target.Add(hordeMonsterList[0]);
-    //                    break;
-    //            }
-    //            break;
+//                case TargetMonsterField.BOTH:
+//                    //Add left most to the list
+//                    target.Add(playerMonsterList[0]);
+//                    //Add left most to the list
+//                    target.Add(hordeMonsterList[0]);
+//                    break;
+//            }
+//            break;
 
-    //        //Rightmost
-    //        case WhichEdge.RIGHTMOST:
-    //            switch (targetSideOfField)
-    //            {
-    //                case TargetMonsterField.PLAYER:
-    //                    //Add left most to the list
-    //                    target.Add(playerMonsterList[playerMonsterList.Count-1]);
-    //                    break;
+//        //Rightmost
+//        case WhichEdge.RIGHTMOST:
+//            switch (targetSideOfField)
+//            {
+//                case TargetMonsterField.PLAYER:
+//                    //Add left most to the list
+//                    target.Add(playerMonsterList[playerMonsterList.Count-1]);
+//                    break;
 
-    //                case TargetMonsterField.HORDE:
-    //                    //Add left most to the list
-    //                    target.Add(hordeMonsterList[hordeMonsterList.Count - 1]);
-    //                    break;
+//                case TargetMonsterField.HORDE:
+//                    //Add left most to the list
+//                    target.Add(hordeMonsterList[hordeMonsterList.Count - 1]);
+//                    break;
 
-    //                case TargetMonsterField.BOTH:
-    //                    //Add left most to the list
-    //                    target.Add(playerMonsterList[playerMonsterList.Count - 1]);
-    //                    //Add left most to the list
-    //                    target.Add(hordeMonsterList[hordeMonsterList.Count - 1]);
-    //                    break;
-    //            }
-    //            break;
-    //    }
-    //    //Debug.Log("Targetable Player Monsters = "+ playerMonsterList.Count);
-    //    //Debug.Log("Targetable Horde Monsters = " + playerMonsterList.Count);
-    //    //Debug.Log("Current Target LIST - " + target);
-    //    //Debug.Log("Current Target Array - "+ target.ToArray());
+//                case TargetMonsterField.BOTH:
+//                    //Add left most to the list
+//                    target.Add(playerMonsterList[playerMonsterList.Count - 1]);
+//                    //Add left most to the list
+//                    target.Add(hordeMonsterList[hordeMonsterList.Count - 1]);
+//                    break;
+//            }
+//            break;
+//    }
+//    //Debug.Log("Targetable Player Monsters = "+ playerMonsterList.Count);
+//    //Debug.Log("Targetable Horde Monsters = " + playerMonsterList.Count);
+//    //Debug.Log("Current Target LIST - " + target);
+//    //Debug.Log("Current Target Array - "+ target.ToArray());
 
-    //    return target.ToArray();
-    //}
+//    return target.ToArray();
+//}
