0035307ca62d75090e4ea665303136db879b2c3b
Jax <jaxompellow@gmail.com>
2024-10-17 17:15:15 +1100

    Implemented Title Screen

---
+++ Against the Horde/Assets/Scripts/_Managers/SceneLoaderManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderManager : MonoBehaviour
+{
+    public void LoadTitleScene()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+
+    public void LoadCombatScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+
+}
